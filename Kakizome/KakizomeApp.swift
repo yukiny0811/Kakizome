73e54093527e0df637a1aa0884dba51004bb622b
yukiny0811 <yukiny0811@gmail.com>
2024-01-27 13:05:42 +0900

    Initial Commit

---
+++ Kakizome/KakizomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KakizomeApp.swift
+//  Kakizome
+//
+//  Created by Yuki Kuwashima on 2024/01/27.
+//
+
+import SwiftUI
+
+@main
+struct KakizomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
