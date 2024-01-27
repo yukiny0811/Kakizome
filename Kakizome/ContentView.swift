36e9d12300a081f101d8fe06780e71e98117ca8a
yukiny0811 <yukiny0811@gmail.com>
2024-01-27 17:26:39 +0900

    kakizome

--- Kakizome/ContentView.swift
+++ Kakizome/ContentView.swift
@@ -6,19 +6,61 @@
 //
 
 import SwiftUI
+import CoreMotion
+import SwiftyCreatives
 
 struct ContentView: View {
+    
+    let sketch = KakizomeSketch()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            ConfigurableSketchView<DefaultOrthoConfig, MainDrawConfig>(sketch)
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+class KakizomeDrawConfig: DrawConfigBase {
+    static var contentScaleFactor: Int = 3
+    static var blendMode: SwiftyCreatives.BlendMode = .normalBlend
+    static var clearOnUpdate: Bool = false
+    static var frameRate: Int = 60
+}
+
+class KakizomeSketch: Sketch {
+    
+    private let motionManager = CMMotionManager()
+    
+    var points: [f3] = []
+    
+    var currentPos: f3 = .zero
+    
+    override init() {
+        super.init()
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.deviceMotionUpdateInterval = 0.001
+            
+            motionManager.startDeviceMotionUpdates(to: OperationQueue.main) { [weak self] data, error in
+                guard let self else { return }
+                guard let data = data else { return }
+                let userAcceleration = data.userAcceleration
+                let acc = f3.init(
+                    -Float(userAcceleration.x),
+                    -Float(userAcceleration.y),
+                    0
+                )
+                currentPos += acc
+                points.append(currentPos)
+            }
+        }
+    }
+    
+    override func draw(encoder: SCEncoder) {
+        color(1, 1, 1, 1)
+        
+        for p in points {
+            rect(p * 100, 10, 10)
+        }
+    }
+    
 }
